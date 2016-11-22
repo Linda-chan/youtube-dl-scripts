6633ade992e556a040f5dd8fe8fac2111ae25c94
Linda Kaioh <lindaoneesama@gmail.com>
2016-11-22 04:02:27 +0300

    CRLF test =_=

--- youtube-dl-np-c0.bat
+++ youtube-dl-np-c0.bat
@@ -7,4 +7,28 @@
   @GOTO :EOF
 )
 
-@"%~d0%~p0youtube-dl.exe" "%~1" --proxy "" --format best --netrc --all-subs --force-ipv4 --xattrs
+:: Main options...
+@SET PARAMS=%PARAMS% --netrc
+@SET PARAMS=%PARAMS% --force-ipv4
+@SET PARAMS=%PARAMS% --buffer-size 2097152
+
+:: Fanservice options...
+@SET PARAMS=%PARAMS% --xattrs
+@SET PARAMS=%PARAMS% --add-metadata
+
+@SET PARAMS=%PARAMS% --all-subs
+@SET PARAMS=%PARAMS% --embed-subs
+
+@SET PARAMS=%PARAMS% --write-thumbnail
+@SET PARAMS=%PARAMS% --embed-thumbnail
+
+:: Test options...
+::@SET PARAMS=%PARAMS% --dump-user-agent
+::@SET PARAMS=%PARAMS% --include-ads
+
+:: Rare options...
+::@SET PARAMS=%PARAMS% --write-description
+::@SET PARAMS=%PARAMS% --write-info-json
+::@SET PARAMS=%PARAMS% --write-annotations
+
+@"%~d0%~p0youtube-dl.exe" "%~1" --proxy "" --format best %PARAMS%
