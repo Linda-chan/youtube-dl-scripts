ae3f3ff25d0a9b0828927d1aef4b7cddcf91aeef
Linda Kaioh <lindaoneesama@gmail.com>
2016-11-22 06:57:29 +0300

    Fixed other Linux scripts

--- youtube-dl-np-ytcombo.sh
+++ youtube-dl-np-ytcombo.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-MY_RP=$( readlink -e "$0" )
-MY_DP=$( dirname "$MY_RP" )
-
+# Show usage if no URLs provided...
 if [ -z "$1" ] ; then
   echo Usage: $0 video-or-playlist-url
   exit
 fi
 
+# Figure out script's directory...
+MY_RP=$( readlink -e "$0" )
+MY_DP=$( dirname "$MY_RP" )
+
+# Set HOME to script's directory...
+HOME=$MY_DP
+
+# Call other BAT files in script's directory...
 "$MY_DP/youtube-dl-np-c9.sh" "$1"
 "$MY_DP/youtube-dl-np-c5.sh" "$1"
 "$MY_DP/youtube-dl-np-c0.sh" "$1"
