5ecdb1d106acfcfa17e06c7d28e7e3062cfe0af3
Linda Kaioh <lindaoneesama@gmail.com>
2017-01-11 12:46:40 +0300

    Added updater version without proxy

---
+++ youtube-dl-np-update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Figure out script's directory...
+MY_RP=$( readlink -e "$0" )
+MY_DP=$( dirname "$MY_RP" )
+
+# Set HOME to script's directory...
+HOME=$MY_DP
+
+# Call main program...
+sudo youtube-dl --proxy "" -U
+
+# Generate usage in text file...
+youtube-dl --help > $HOME"/youtube-dl.txt"
