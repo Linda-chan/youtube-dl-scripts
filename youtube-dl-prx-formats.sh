e1df7c8a2adb5c0475b006633c6cf96479c13458
Linda Kaioh <lindaoneesama@gmail.com>
2017-03-26 18:32:04 +0300

    Scripts for list available formats were added

---
+++ youtube-dl-prx-formats.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Show usage if no URLs provided...
+if [ -z "$1" ] ; then
+  echo Usage: $0 video-or-playlist-url [video-or-playlist-url [...]]
+  exit
+fi
+
+# Figure out script's directory...
+MY_RP=$( readlink -e "$0" )
+MY_DP=$( dirname "$MY_RP" )
+
+# Set HOME to script's directory...
+export HOME=$MY_DP
+export XDG_CACHE_HOME=$MY_DP
+
+# Main options...
+PARAMS=$PARAMS" --netrc"
+PARAMS=$PARAMS" --force-ipv4"
+PARAMS=$PARAMS" --buffer-size 2097152"
+
+# Fanservice options...
+PARAMS=$PARAMS" --xattrs"
+PARAMS=$PARAMS" --add-metadata"
+
+PARAMS=$PARAMS" --all-subs"
+PARAMS=$PARAMS" --embed-subs"
+
+PARAMS=$PARAMS" --write-thumbnail"
+PARAMS=$PARAMS" --embed-thumbnail"
+
+PARAMS=$PARAMS" --console-title"
+
+# Don't stop if program is unable to import thumbnail...
+PARAMS=$PARAMS" --ignore-errors"
+
+# Set timeout to 180 sec...
+PARAMS=$PARAMS" --socket-timeout 180"
+
+# Rare options...
+#PARAMS=$PARAMS" --write-description"
+#PARAMS=$PARAMS" --write-info-json"
+#PARAMS=$PARAMS" --write-annotations"
+
+# Change rights on .netrc file for youtube-dl pleasure...
+chmod 600 "$MY_DP/.netrc"
+
+# Call main program...
+youtube-dl --list-formats $PARAMS "$@"
